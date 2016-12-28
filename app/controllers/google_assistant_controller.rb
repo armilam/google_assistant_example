a4b6603c3bbfc7121eefad7bba7c2d613bb85be0
Aaron Milam <aaron@lesson.ly>
2016-12-28 16:36:55 -0500

    Add an ask

--- app/controllers/google_assistant_controller.rb
+++ app/controllers/google_assistant_controller.rb
@@ -6,11 +6,13 @@ class GoogleAssistantController < ApplicationController
     assistant_response = GoogleAssistant.new(params).respond_to do |assistant|
       assistant.intent.main do
         logger.debug("main intent")
-        assistant.tell("<speak>I can speak!</speak>")
+        input_prompt = assistant.build_input_prompt(true, "<speak>Say something please?</speak>", ["<speak>What was that?</speak>"])
+        assistant.ask(input_prompt, { "this is data" => "yes, it is"})
       end
 
       assistant.intent.text do
         logger.debug("text intent")
+        assistant.tell("<speak>I heard you!</speak>")
       end
     end
 
