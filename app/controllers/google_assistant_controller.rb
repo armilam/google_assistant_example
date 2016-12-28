7f27a3beb639107d2168b04dd3aef3884f42986c
Aaron Milam <aaron@lesson.ly>
2016-12-28 17:22:11 -0500

    Make it work

--- app/controllers/google_assistant_controller.rb
+++ app/controllers/google_assistant_controller.rb
@@ -2,12 +2,13 @@ class GoogleAssistantController < ApplicationController
 
   def conversation
     puts params.as_json
+    response.headers["Google-Assistant-API-Version"] = "v1"
 
     assistant_response = GoogleAssistant.new(params).respond_to do |assistant|
       assistant.intent.main do
         logger.debug("main intent")
         input_prompt = assistant.build_input_prompt(true, "<speak>Say something please?</speak>", ["<speak>What was that?</speak>"])
-        assistant.ask(input_prompt, { "this is data": "yes, it is"}.to_json)
+        assistant.ask(input_prompt)
       end
 
       assistant.intent.text do
