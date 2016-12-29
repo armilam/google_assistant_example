c0ce8327c03f6727dc1b1469454050cd8f6a26d5
Aaron Milam <aaron@lesson.ly>
2016-12-28 19:56:07 -0500

    Log the conversation token

--- app/controllers/google_assistant_controller.rb
+++ app/controllers/google_assistant_controller.rb
@@ -4,14 +4,15 @@ class GoogleAssistantController < ApplicationController
     puts params.as_json
 
     assistant_response = GoogleAssistant.new(params, response).respond_to do |assistant|
+
+      puts assistant.conversation.token
+
       assistant.intent.main do
-        logger.debug("main intent")
         input_prompt = assistant.build_input_prompt(true, "<speak>Say something please?</speak>", ["<speak>What was that?</speak>"])
         assistant.ask(input_prompt)
       end
 
       assistant.intent.text do
-        logger.debug("text intent")
         assistant.tell("<speak>I heard you!</speak>")
       end
     end
